7837c1c1811463a01d8a797271bdc8217451c7fc
a.atta <143314426+AbdelrhmanAtta@users.noreply.github.com>
2026-09-02 10:15:10 +0300

    Feat: Initial binary loader (#20)
    * [Chore] Added script to install dependencies, modfied cmake to run compiled lief
    
    [Docs] Modified README.md to include running lief script
    
    * [Feat] Added binary loader initial logic
    
    * [CI] Added unit_tests/test_binary_loader.cpp placeholder to suppress ci error

--- dev/src/main.cpp
+++ dev/src/main.cpp
@@ -1,19 +1,2 @@
-#include <iostream>
-
-#include "calc.hpp"
-
 int main() {
-    test::Calculator calc(5.0f, 10.0f);
-
-    calc.add();
-    float res = 0.0f;
-    calc.getResult(&res);
-    std::cout << "5 + 10 = " << res << "\n";
-
-    calc.setOperands(5.0f, 10.0f);
-    calc.divide();
-    calc.getResult(&res);
-    std::cout << "5 / 10 = " << res << "\n";
-
-    return 0;
 }
