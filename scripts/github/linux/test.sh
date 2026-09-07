a485af611645cd91fcc74a0e6c511986b223068c
abadr99 <abdelrhmanezzbadr@outlook.com>
2026-09-08 00:15:33 +0300

    Fix unittests binary

--- scripts/github/linux/test.sh
+++ scripts/github/linux/test.sh
@@ -6,4 +6,4 @@ echo "--- Running Raw Google Tests (Linux) ---"
 SCRIPT_PATH="$(cd "$(dirname -- "$0")" && pwd -P)"
 PROJECT_ROOT="$(cd "$SCRIPT_PATH/../../.." && pwd -P)"
 
-"$PROJECT_ROOT/build-linux/tests/unit_tests" --gtest_shuffle
+"$PROJECT_ROOT/build-linux/tests/unit_tests/unit_tests" --gtest_shuffle
