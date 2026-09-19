e4dc5f30a75ea213b85ef5d023142359a47f608e
AbdelrhmanAtta <abdelrhmatta@gmail.com>
2026-09-19 15:47:05 +0300

    [Feature] Added helper functions for ssa arch

--- dev/inc/frontend/IrBuilder.hpp
+++ dev/inc/frontend/IrBuilder.hpp
@@ -1,8 +1,14 @@
 #ifndef DEV_INC_FRONTEND_IRBUILDER_HPP_
 #define DEV_INC_FRONTEND_IRBUILDER_HPP_
 
+#include <cstdint>
+#include <map>
+
+#include "frontend/Instruction.hpp"
+#include "frontend/IrBasicBlock.hpp"
 #include "frontend/IrFunction.hpp"
 #include "frontend/ControlFlowGraph.hpp"
+#include "frontend/IrInstruction.hpp"
 
 namespace rosetta {
 namespace frontend {
@@ -26,7 +32,15 @@ class IrBuilder {
   void LiftCall(const instruction::Instruction& inst, IrBasicBlock& dst);
   void LiftReturn(const instruction::Instruction& inst, IrBasicBlock& dst);
 
+  // Helpers
+  IrOperand LowerOperand(const instruction::InstructionOperand& operand, IrBasicBlock& dst);
+  IrOperand LowerMemoryOperand(const instruction::InstructionOperand& operand, IrBasicBlock& dst);
+  IrOpcode MapOpcode(const instruction::Instruction& inst) const;
+  IrOperand NewVirtualRegister(uint32_t reg, uint8_t size_bits);
+
   cfg::ControlFlowGraph graph_;
+  std::map<uint32_t, IrOperand> register_map_;
+  uint64_t next_vreg_int_ = 0;
 };
 }  // namespace rosetta
 }  // namespace frontend
